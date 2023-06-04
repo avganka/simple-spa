4d2253d42398814981ca17fd609985199e867b92
avganka <karambasterg@gmail.com>
2023-06-05 00:39:58 +0300

    Добавлен header для всего приложения

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import {Route, Routes} from 'react-router-dom';
+import {Container} from 'react-bootstrap';
+
 import MainPage from './pages/MainPage';
 import AboutPage from './pages/AboutPage';
 import DetailPage from './pages/UserPage';
+import Header from './components/Header';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<MainPage />} />
-      <Route path='/about' element={<AboutPage />} />
-      <Route path='/user/:userId' element={<DetailPage />} />
-    </Routes>
+    <>
+      <Header />
+      <main className='mt-4 mb-5'>
+        <Container>
+          <Routes>
+            <Route path='/' element={<MainPage />} />
+            <Route path='/about' element={<AboutPage />} />
+            <Route path='/user/:userId' element={<DetailPage />} />
+          </Routes>
+        </Container>
+      </main>
+    </>
   );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,38 @@
+import {Container, Nav, Navbar, Offcanvas} from 'react-bootstrap';
+import {NavLink, Link} from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+      <Navbar bg='light' variant='light' expand={'lg'} className='shadow-sm mb-3' sticky='top'>
+        <Container>
+          <Navbar.Toggle aria-controls='offcanvasNavbar-expand-lg' />
+          <Navbar.Brand to='/' as={Link}>
+            Simple SPA
+          </Navbar.Brand>
+          <Navbar.Offcanvas
+            id='offcanvasNavbar-expand-lg'
+            aria-labelledby='offcanvasNavbarLabel-expand-lg'
+            placement='start'
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title> Simple SPA</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className='justify-content-start flex-grow-1 pe-3'>
+                <Nav.Link as={NavLink} to='/'>
+                  Список постов
+                </Nav.Link>
+                <Nav.Link as={NavLink} to='/about'>
+                  Обо мне
+                </Nav.Link>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    </header>
+  );
+}
+
+export default Header;
