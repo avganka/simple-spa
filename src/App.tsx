e288784fe553f72e7e93e25436679c0a5e04135c
avganka <karambasterg@gmail.com>
2023-06-05 23:37:54 +0300

    Добавлена страница Not Found

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import MainPage from './pages/MainPage';
 import AboutPage from './pages/AboutPage';
 import DetailPage from './pages/UserPage';
 import Header from './components/Header';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <main className='mt-4 mb-5'>
         <Container>
           <Routes>
+            <Route path='*' element={<NotFound />} />
             <Route path='/' element={<MainPage />} />
             <Route path='/about' element={<AboutPage />} />
             <Route path='/user/:userId' element={<DetailPage />} />
---
+++ src/components/NotFound.tsx
@@ -0,0 +1,10 @@
+function NotFound() {
+  return (
+    <>
+      <h1>404</h1>
+      <p>Такой страницы не существует</p>
+    </>
+  );
+}
+
+export default NotFound;
